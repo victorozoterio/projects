d0c60ce6f869dc116df136435f2a30e485a66ad0
Victor Ozoterio <victorozoterio@gmail.com>
2025-06-02 23:07:23 -0300

    feat: configurações iniciais do management-backend

---
+++ apps/url-shortener/backend/management-backend/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'url-shortener-management-backend',
+  preset: '../../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  coverageDirectory: '../coverage/apps/url-shortener/backend/management-backend',
+};
---
+++ apps/url-shortener/backend/management-backend/src/main.ts
@@ -0,0 +1,17 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { swaggerConfig } from './config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  SwaggerModule.setup('docs', app, SwaggerModule.createDocument(app, swaggerConfig));
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+  await app.listen(port);
+  const logger = new Logger('Bootstrap');
+  logger.log(`Application is running on: http://localhost:${port}`);
+}
+
+bootstrap();
