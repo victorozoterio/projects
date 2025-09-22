15232f46fb393100d9ed93071f215226fc2b6eaf
Victor Ozoterio <victorozoterio@gmail.com>
2025-09-22 14:39:59 -0300

    fix: remove middleware desnecessário

--- apps/investments-price-alert/backend/root-backend/src/app.module.ts
+++ apps/investments-price-alert/backend/root-backend/src/app.module.ts
@@ -1,12 +1,11 @@
 import { HealthzModule, XApiKeyMiddleware } from '@projects/shared/backend';
-import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { databaseConfig, envConfig } from './config';
 import { InvestmentsModule } from './modules/investments/investments.module';
-import { UserUuidMiddleware } from './middlewares';
 import { UserEntity } from './modules/users/entities/user.entity';
 
 @Module({
@@ -25,12 +24,5 @@ import { UserEntity } from './modules/users/entities/user.entity';
 export class AppRootModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(XApiKeyMiddleware).exclude('healthz', 'auth/(.*)').forRoutes('*');
-    consumer
-      .apply(UserUuidMiddleware)
-      .exclude('healthz', 'auth/(.*)', 'users', 'users/(.*)', {
-        path: 'investments/(.*)',
-        method: RequestMethod.GET,
-      })
-      .forRoutes('*');
   }
 }
--- apps/investments-price-alert/backend/root-backend/src/strategies/jwt.strategy.ts
+++ apps/investments-price-alert/backend/root-backend/src/strategies/jwt.strategy.ts
@@ -1,6 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserEntity } from '../modules/users/entities/user.entity';
+import { Repository } from 'typeorm';
 
 interface JwtPayload {
   sub: string;
@@ -11,7 +14,10 @@ interface JwtPayload {
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly users: Repository<UserEntity>,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -19,7 +25,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: JwtPayload) {
-    return { uuid: payload.sub, email: payload.email };
+  async validate(payload: JwtPayload): Promise<UserEntity> {
+    const user = await this.users.findOneBy({ uuid: payload.sub });
+    if (!user) throw new NotFoundException('User does not exist.');
+    return user;
   }
 }
